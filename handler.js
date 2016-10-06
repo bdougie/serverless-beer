20e5d02b056240917ca1f0e4294c9348842403ad
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-10-06 11:00:39 -0700

    fixes my mistakes

--- handler.js
+++ handler.js
@@ -11,18 +11,18 @@ module.exports.hello = (event, context, cb) => cb(null,
 // You can add more handlers here, and reference them
 module.exports.addRating = (event, context, callback) => {
   var docClient = new AWS.DynamoDB.DocumentClient();
+
+  var Item = {
+      id: uuid.v4(),
+      beer: event.body.beer,
+      rating: Number(event.body.rating)
+  };
+
+
+  docClient.put({TableName: 'slsbeer', Item: Item}, (err, data) => {
+      if (err) {
+        callback(err);
+      }
+      callback(null, { data: data });
+  });
 }
-
-var Item = {
-    id: uuid.v4(),
-    beer: event.body.beer,
-    rating: Number(event.body.rating)
-};
-
-
-docClient.put({TableName: 'slsbeer', Item: Item}, (err, data) => {
-    if (err) {
-      callback(err);
-    }
-    callback(null, { data: data });
-});
